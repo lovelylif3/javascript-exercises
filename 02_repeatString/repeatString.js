8f45be53e2da33b3fb01444b0770d88585d93943
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-11 12:40:53 -0500

    Add handling for negative numbers and 0

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,9 +2,15 @@ let result = "";
 
 function repeatString(string, num) {
     result = string
+    if (num === 0) {
+        result = ''
+    } else if (num < 0) {
+        result = "ERROR"
+    }
+
     for (let i = 0; i < num - 1; i++) {
     result += string;
-}
+    }
 return result;
 }
 

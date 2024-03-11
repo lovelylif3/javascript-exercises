15b184c2021a8297bd2442656c842b1b038979eb
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-10 21:04:13 -0500

    Add first repeater loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+let result = "";
 
-};
+function repeatString(string, num) {
+    for (let i = 0; i < 3; i++) {
+    result += string
+}
+return result
+}
+
+
 
 // Do not edit below this line
 module.exports = repeatString;

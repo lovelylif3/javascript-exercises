2b6aa27c363a95f48e7ef671353f2e07eb112480
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-11 13:18:10 -0500

    Create function to remove single element from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+function removeFromArray(arrayProvided, toRemove) {
+    let indexToRemove = arrayProvided.indexOf(toRemove);
+    if (indexToRemove > -1) { // only splice array when item is found
+        arrayProvided.splice(indexToRemove, 1); 
+    }
+    return arrayProvided;
+}
 
-};
+//Find the index of the array element you want to remove using indexOf, 
+//and then remove that index with splice
 
 // Do not edit below this line
 module.exports = removeFromArray;

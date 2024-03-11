8e946bb3b1110c601241008bede37dcf4688df94
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-11 13:03:10 -0500

    Write function to reverse a string Function converts string into array, reverses it and converts back to string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+let givenString
 
-};
+function reverseString(givenString) {
+    let splitString = givenString.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
+}
 
+
+//note: need to convert string into array then back into string
 // Do not edit below this line
 module.exports = reverseString;

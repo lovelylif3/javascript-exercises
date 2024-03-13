7c82b14924732978ad4d592f86f5cb29dc557ddc
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-12 22:08:18 -0500

    Create function to sum values in a given range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sumCalc = 0
+    for (let step = x; step <= y; step++) {
+        // Runs 5 times, with values of step 0 through 4.
+        sumCalc += step
+      }
+    return sumCalc
 };
 
 // Do not edit below this line

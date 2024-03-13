dc4ccb2aaf61591b678e982db1b16c36375b75d8
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-13 18:40:41 -0500

    Add checks for non-number variables

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,25 @@
 const sumAll = function(x, y) {
     let sumCalc = 0
-    for (let step = x; step <= y; step++) {
-        // Runs 5 times, with values of step 0 through 4.
-        sumCalc += step
-      }
+    
+    if  ((x < 0 || y < 0) || 
+        (isNaN(x) || isNaN(y)) || 
+        ((typeof(x) === 'string') || (typeof(y) === 'string'))) {
+            return "ERROR"
+        } 
+        
+    else {
+
+        if (x > y) {
+            for (let step = y; step <= x; step++) {
+                sumCalc += step
+            } 
+        } else {
+            for (let step = x; step <= y; step++) {
+                sumCalc += step
+            }
+        }
+        
+    }
     return sumCalc
 };
 

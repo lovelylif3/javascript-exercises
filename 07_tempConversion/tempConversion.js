0cee45b2505d8a68b524c7c7df321fdaa95e5899
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-13 19:42:59 -0500

    Create functions for temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let unrounded = (x - 32) * (5/9)
+  return Math.round(unrounded * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let unrounded = x * (9/5) + 32
+  return Math.round(unrounded * 10) / 10
 };
 
 // Do not edit below this line
